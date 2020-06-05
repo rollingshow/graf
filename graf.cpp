e02cf59b4d94683798307e67d5854a977228d10c
rollingshow <46083919+rollingshow@users.noreply.github.com>
2020-06-05 23:19:01 +0800

    Add files via upload

---
+++ graf.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "graf.h"
+
+
+void Graph::removeNode(Node* node) {
+	nodes.erase(node);
+	// Remove also from all neighbours list
+	for (std::set<Node*>::iterator it = nodes.begin();
+		it != nodes.end(); it++) {
+		(*it)->removeNeighbour(node);
+	}
+}
+void Graph::addEdge(Node* begin, Node* end) {
+	if (nodes.find(begin) == nodes.end())
+		return;
+	if (nodes.find(end) == nodes.end())
+		return;
+	begin->addNeighbour(end);
+	end->addNeighbour(begin);
+}
+
+bool BFS::connected(Node* begin, Node* end) {
+	std::queue<Node*> nodes; nodes.push(begin);
+	std::set<Node*> visited;
+	while (!nodes.empty()) {
+		Node* next = nodes.front(); nodes.pop();
+		if (end == next) return true;
+		visited.insert(next);
+		for (node_iterator it = next->nb_begin();
+			it != next->nb_end(); it++)
+			if (visited.find(*it) == visited.end())
+				nodes.push(*it);
+	}
+	return false;
+}
+
+static Way unroll(std::map<Node*, MarkedNode> visited,
+	Node* begin, Node* curr) {
+	Way way;
+	way.length = visited[curr].mark;
+	while (curr != begin) {
+		way.nodes.push_back(curr);
+		curr = visited[curr].prev;
+	}
+	way.nodes.push_back(begin);
+	return way;
+}
+
---
+++ graf.h
@@ -0,0 +1,84 @@
+#pragma once
+#include <iostream>
+#include <set>
+#include <vector>
+#include <set>
+#include <string.h>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+typedef set<Node*>::const_iterator node_iterator;
+class Graph {
+	std::set<Node*> nodes;
+public:
+	void addNode(Node* node);
+	void removeNode(Node* node);
+	void addEdge(Node* begin, Node* end);
+	void removeEdge(Node* begin, Node* end);
+	node_iterator begin() const {
+		return nodes.begin();
+	}
+	node_iterator end() const { return nodes.end(); }
+};
+
+class Node {
+	std::string name;
+	std::set<Node*> neighbours;
+	void addNeighbour(Node* neighbour);
+	void removeNeighbour(Node* neighbour);
+public:
+	Node(const std::string& aname) : name(aname) {}
+	const std::string& getName() const { return name; }
+	node_iterator nb_begin() const {
+		return neighbours.begin();
+	}
+	node_iterator nb_end() const { return neighbours.end(); }
+	friend class Graph;
+};
+class BFS {
+	const Graph& graph;
+public:
+	BFS(const Graph& agraph) : graph(agraph) {}
+	bool connected(Node* begin, Node* end);
+};
+
+struct MarkedNode {
+	Node* node; int mark;
+	Node* prev;
+	MarkedNode(Node* anode = 0, int amark = 0, Node* aprev = 0) :
+		node(anode), mark(amark), prev(aprev) {}
+};
+class PriorityQueue {
+	std::vector<MarkedNode> nodes;
+public:
+	MarkedNode pop();
+	void push(Node* node, int mark, Node* prev);
+	bool empty() const { return nodes.empty(); }
+};MarkedNode PriorityQueue::pop() {
+	MarkedNode mn = nodes.back();
+	nodes.pop_back();
+	return mn;
+}
+void PriorityQueue::push(Node* node, int mark, Node* prev) {
+	std::vector<MarkedNode>::iterator it = nodes.begin();
+	MarkedNode mn(node, mark, prev);
+	// From higher to lower
+	while (it != nodes.end() && mark < it->mark) it++;
+	if (it == nodes.end()) nodes.push_back(mn);
+	else nodes.insert(it, mn);
+}
+
+struct Way {
+	std::vector<Node*> nodes;
+	int length;
+	Way() : length(-1) {}
+};
+class Dijkstra {
+	const Graph& graph;
+public:
+	Dijkstra(const Graph& agraph) : graph(agraph) {}
+	Way shortestWay(Node* begin, Node* end);
+};
+
